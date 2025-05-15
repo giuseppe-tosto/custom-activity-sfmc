c250def11fcc31beb71a507449fd7018690b5707
giuseppe-tosto <116282681+giuseppe-tosto@users.noreply.github.com>
2025-05-15 15:34:55 +0200

    Update execute.js

--- api/execute.js
+++ api/execute.js
@@ -1,10 +1,9 @@
-export default function handler(req, res) {
+module.exports = (req, res) => {
   if (req.method === "POST") {
     const email = req.body?.inArguments?.[0]?.Email || "";
     const isBlocked = (email === "giuseppe.tosto@skylabs.it");
-
     return res.status(200).json({ branchResult: isBlocked ? "NO" : "YES" });
-  } else {
-    return res.status(405).send("Method Not Allowed");
   }
-}
+  res.setHeader("Allow", "POST");
+  return res.status(405).send("Method Not Allowed");
+};
