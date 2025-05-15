c46f3b9600fc89d29a4dad4874a072f094ff35c1
giuseppe-tosto <116282681+giuseppe-tosto@users.noreply.github.com>
2025-05-15 15:56:42 +0200

    Delete api directory

--- api/execute.js
+++
@@ -1,9 +0,0 @@
-module.exports = (req, res) => {
-  if (req.method === "POST") {
-    const email = req.body?.inArguments?.[0]?.Email || "";
-    const isBlocked = (email === "giuseppe.tosto@skylabs.it");
-    return res.status(200).json({ branchResult: isBlocked ? "NO" : "YES" });
-  }
-  res.setHeader("Allow", "POST");
-  return res.status(405).send("Method Not Allowed");
-};
