466b6eac2ebdae43a0dfbb590e363248c0aa96f5
giuseppe-tosto <116282681+giuseppe-tosto@users.noreply.github.com>
2025-05-16 15:44:54 +0200

    Update index.js

--- public/index.js
+++ public/index.js
@@ -2,15 +2,22 @@
   'use strict';
 
   var connection = new Postmonger.Session();
+  // qui conserviamo la stringa di personalizzazione per EmailAddress
+  var emailToken = '';
   // hard-coded per semplicità
   var HARD_SINGLE = 'giuseppe.tosto@skylabs.it';
   var HARD_DOMAIN = 'yahoo.it';
   
-  // initActivity: prepopola le radio e abilita Done
+  // 1) initActivity: leggo gli inArguments e popolo emailToken + radio
   connection.on('initActivity', function(data) {
-    var inArgs = (data.arguments && data.arguments.execute && data.arguments.execute.inArguments)
+    var inArgs = (data.arguments &&
+                  data.arguments.execute &&
+                  data.arguments.execute.inArguments)
                  ? Object.assign({}, ...data.arguments.execute.inArguments)
                  : {};
+
+    // salvo la stringa mustache o il valore passato dal manifest
+    emailToken = inArgs.EmailAddress || '';
 
     var single = Boolean(inArgs.SingleEmails);
     var domain = Boolean(inArgs.DomainList);
@@ -21,15 +28,15 @@
     Array.prototype.forEach.call(
       document.getElementsByName('filterType'),
       function(radio){
-        radio.checked = radio.value === type;
+        radio.checked = (radio.value === type);
       }
     );
 
-    // Segnalo a JB che la UI è pronta e abilita “Done”
+    // dico a Journey Builder che la UI è pronta e abilito “Done”
     connection.trigger('ready');
   });
 
-  // Quando l’utente clicca Done/Next in Journey Builder
+  // 2) clickedNext: ricreo TUTTI gli inArguments, includendo EmailAddress
   connection.on('clickedNext', function() {
     var selected = document.querySelector('input[name=filterType]:checked').value;
 
@@ -37,15 +44,19 @@
       arguments: {
         execute: {
           inArguments: [
-            { SingleEmails: selected === 'domain' ? '' : HARD_SINGLE },
-            { DomainList:   selected === 'single' ? '' : HARD_DOMAIN }
+            // 1) EmailAddress (placeholder mustache prelevato da emailToken)
+            { EmailAddress: emailToken },
+            // 2) SingleEmails / DomainList in base alla scelta
+            { SingleEmails: (selected === 'domain' ? '' : HARD_SINGLE) },
+            { DomainList:   (selected === 'single' ? '' : HARD_DOMAIN) }
           ]
         }
       },
       metaData: { isConfigured: true }
     };
 
+    // invio la configurazione aggiornata a JB
     connection.trigger('updateActivity', payload);
   });
-   connection.trigger('ready');
+
 })();
